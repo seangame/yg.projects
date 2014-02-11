06198090c6fe364aa4258f03bf26e5ba5e7ffbdc
Jason R. Coombs <jaraco@jaraco.com>
2014-02-11 15:34:25 -0500

    Always return something (even on success). Remove validation on casetaskevent and memo, which aren't required.

--- timesheets.js
+++ timesheets.js
@@ -1,14 +1,13 @@
 // borrowed from http://blog.prolecto.com/2011/12/18/netsuite-restlet-sample-program-exploits-new-power/
 
 function CreateTimebills(data_in) {
-	var output = '';
 	//nlapiLogExecution('DEBUG','createRecord',(typeof data_in.timebill));
+	var result = new Object();
 	var msg = validateTimeBills(data_in);
 	if (msg) {
-		var err = new Object();
-		err.status = "failed";
-		err.message = msg;
-		return err;
+		result.status = "failed";
+		result.message = msg;
+		return result;
 	}
 	var timebills = data_in.timebill;
 	for (var timebillobject in timebills) {
@@ -27,7 +26,8 @@ function CreateTimebills(data_in) {
 		var timebillid = nlapiSubmitRecord(timebill);
 		nlapiLogExecution('DEBUG', 'Timebill ' + timebillid + ' successfully created', timebillid);
 	}
-	return;
+	result.status = "success";
+	return result;
 }
 
 function validateTimeBills(data_in) {
@@ -46,14 +46,8 @@ function validateTimeBills(data_in) {
 		if (customer == '') {
 			returnMessage += "Customer entry cannot be blank.'\n";
 		}
-		if (casetaskevent == '') {
-			returnMessage += "Case Task Event entry cannot be blank.'\n";
-		}
 		if (hours == '') {
 			returnMessage += "Hours cannot be blank.'\n";
-		}
-		if (memo == '') {
-			returnMessage += "Memo cannot be blank.'\n";
 		}
 	}
 	if (returnMessage)
