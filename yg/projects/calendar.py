0ee82ca13d02d03b9cb4cfcb731010dd1f445d33
Jason R. Coombs <jaraco@jaraco.com>
2014-04-29 16:33:14 -0400

    Add models from my personal scripts

---
+++ yg/projects/calendar.py
@@ -0,0 +1,70 @@
+"""
+YouGov calendar modules
+"""
+
+import datetime
+import functools
+
+import dateutil.relativedelta as rd
+
+class Holiday(datetime.date):
+	def __new__(cls, name, indication, date, weekend_hint=rd.MO(1)):
+		return datetime.date.__new__(cls, date.year, date.month, date.day)
+
+	def __init__(self, name, indication, date, weekend_hint=rd.MO(1)):
+		self.name = name
+		self.indication = indication
+		self.weekend_hint = weekend_hint
+
+	@property
+	def observed(self):
+		delta = rd.relativedelta(weekday=self.weekend_hint)
+		return self + delta if self.weekday() > 4 else self
+
+def holidays_for_year(year):
+	"""
+	Produce the YouGov America holidays for the given year.
+	"""
+	yield Holiday("New Year's Day", "January 1st", datetime.date(year, 1, 1))
+
+	yield Holiday("Martin L King's Birthday", "3rd Monday in January",
+		datetime.date(year, 1, 1) + rd.relativedelta(weekday=rd.MO(3)))
+
+	yield Holiday("President's Day", "3rd Monday in February",
+		datetime.date(year, 2, 1) + rd.relativedelta(weekday=rd.MO(3)))
+
+	yield Holiday("Memorial Day", "Last Monday in May",
+		datetime.date(year, 5, 31) + rd.relativedelta(weekday=rd.MO(-1)))
+
+	yield Holiday("Independence Day", "July 4th", datetime.date(year, 7, 4))
+
+	yield Holiday("Labor Day", "1st Monday in September",
+		datetime.date(year, 9, 1) + rd.relativedelta(weekday=rd.MO(1)))
+
+	tg = Holiday("Thanksgiving Day", "4th Thursday in November",
+		datetime.date(year, 11, 1) + rd.relativedelta(weekday=rd.TH(4)))
+	yield tg
+	yield Holiday("Day after Thanksgiving", "Friday after Thanksgiving",
+		tg + datetime.timedelta(days=1))
+
+	# Christmas Eve and Christmas
+	yield Holiday("Christmas Eve", "December 24",
+		datetime.date(year, 12, 24), rd.FR(-1))
+	yield Holiday("Christmas Day", "December 25",
+		datetime.date(year, 12, 25))
+
+	# New Years Eve
+	yield Holiday("New Year's Eve", "December 31",
+		datetime.date(year, 12, 31), rd.FR(-1))
+
+
+@functools.lru_cache()
+def holidays_for_year_cached(year):
+	return tuple(holidays_for_year(year))
+
+
+def exclude_holidays(days):
+	return (
+		day for day in days
+		if day not in holidays_for_year_cached(day.year)
+	)
---
+++ yg/projects/models.py
@@ -0,0 +1,53 @@
+import csv
+
+class Project:
+	def __init__(self, **params):
+		vars(self).update(params)
+
+	@classmethod
+	def from_dict(cls, d):
+		d = {key.lower().replace(' ', '_'): val for key, val in d.items()}
+		return cls(**d)
+
+	def __repr__(self):
+		return ' '.join((self.id, self.name))
+
+	def __lt__(self, other):
+		return self.name < other.name and len(self.name) < len(other.name)
+
+class Projects(list):
+	@classmethod
+	def from_csv(cls, filename='projects.csv'):
+		with open('projects.csv') as stream:
+			return cls(map(Project.from_dict, csv.DictReader(stream)))
+
+	def best(self, short_name):
+		return next(
+			project
+			for project in sorted(self)
+			if short_name in project.name
+		)
+	__getattr__ = best
+
+class Distribution(dict):
+	"""
+	Provides an overriding distribution
+
+	>>> d = Distribution()
+	>>> d['foo'] = 2
+	>>> d['bar'] = 1
+	foo's portion is 2/3 at a resolution of 1/10
+	>>> d.portion('foo')
+	Decimal('0.7')
+	"""
+	# 1/10 resolution
+	resolution = 10
+
+	@property
+	def total(self):
+		return sum(self.values())
+
+	def portion(self, key, value):
+		ratio = self[key] / self.total
+		portion = ratio*value
+		return round(portion*self.resolution)/self.resolution
