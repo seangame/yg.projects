026d72e90ad6d02365dd247389968c5314bc00d3
Jason R. Coombs <jaraco@jaraco.com>
2014-05-09 08:29:19 -0400

    print the output when the restlet fails

--- test-project-list.py
+++ test-project-list.py
@@ -1,10 +1,11 @@
 import yg.netsuite
+import requests
 cred = yg.netsuite.Credential()
 headers={'Content-Type': 'application/json'}
 headers.update(cred.build_auth_header())
 url = 'https://rest.netsuite.com/app/site/hosting/restlet.nl?script=533&deploy=1'
-import requests
 session = requests.session()
 resp = session.get(url, headers=headers)
 print(resp.ok)
 print(resp.status_code)
+print(resp.text)
