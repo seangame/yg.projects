3aa596123b89f3f07790060dddb41d1b51dc96bb
Jason R. Coombs <jaraco@jaraco.com>
2014-05-16 11:28:20 -0400

    Prevent test-project-list from being executed on collection.

--- test-project-list.py
+++ test-project-list.py
@@ -1,11 +1,16 @@
 import yg.netsuite
 import requests
-cred = yg.netsuite.Credential()
-headers={'Content-Type': 'application/json'}
-headers.update(cred.build_auth_header())
-url = 'https://rest.netsuite.com/app/site/hosting/restlet.nl?script=533&deploy=1'
-session = requests.session()
-resp = session.get(url, headers=headers)
-print(resp.ok)
-print(resp.status_code)
-print(resp.text)
+
+def run():
+	cred = yg.netsuite.Credential()
+	headers={'Content-Type': 'application/json'}
+	headers.update(cred.build_auth_header())
+	url = 'https://rest.netsuite.com/app/site/hosting/restlet.nl?script=533&deploy=1'
+	session = requests.session()
+	resp = session.get(url, headers=headers)
+	print(resp.ok)
+	print(resp.status_code)
+	print(resp.text)
+
+if __name__ == '__main__':
+	run()
