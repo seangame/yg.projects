14b4bc9db95c1e707b83885f66636e769c6eecfd
Jason R. Coombs <jaraco@jaraco.com>
2014-07-16 13:43:27 -0400

    Update demo script to use SB6

--- demo-add-alloc.py
+++ demo-add-alloc.py
@@ -6,11 +6,11 @@ import yg.netsuite as ns
 
 ns.Sandbox.use()
 
-res_alloc_path = '/app/site/hosting/restlet.nl?script=526&deploy=1'
+res_alloc_path = '/app/site/hosting/restlet.nl?script=562&deploy=1'
 url = urllib.parse.urljoin(ns.root, res_alloc_path)
 
 cred = ns.Credential()
-cred.use_admin()
+cred.use_admin('SB6')
 cred.install()
 
 # allocate 50 percent time from Jun 1 through Jul 31, 2014
@@ -18,7 +18,7 @@ cred.install()
 alloc = dict(
 	amount=50,
 	resource_id=271374,
-	type="2", # soft
+	type="2", # soft (use "1" for hard)
 	unit="P", # percent (use "H" for hours)
 	project=270907,
 	start_date=ns.NetSuite.format_date(datetime.date(2014,6,1)),
